c66883f8c9f1683e890279af6e3cc236fde2fd65
robsujunior <robson1144junior@gmail.com>
2025-04-13 17:01:44 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import AddTask from "./components/AddTasks";
+import Tasks from "./components/Tasks";
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks />
+      </div>
+    </div>
+  );
+}
+
+export default App;
